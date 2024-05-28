6a2a7615d8911e00b9584d3351ad463f5ce45902
DarkDeveloper25 <darkdeveloper259@gmail.com>
2024-05-27 19:27:33 -0500

    s2

---
+++ AppDemo/.idea/sonarlint/securityhotspotstore/index.pb
@@ -0,0 +1,5 @@
+
+d
+4app/src/main/java/pe/edu/idat/appdemo/Fundamentos.kt,0\0\00400fe3b7406aec30a8a4099107ab3fd8e2afe2
+e
+5app/src/main/java/pe/edu/idat/appdemo/MainActivity.kt,7\c\7cd2496b875101d46232e374789a415a4fadea3d
---
+++ AppDemo/app/src/main/java/pe/edu/idat/appdemo/Fundamentos.kt
@@ -0,0 +1,56 @@
+package pe.edu.idat.appdemo
+
+import android.util.Log
+
+class Fundamentos {
+
+    var nombres = "Kenshin"
+    var apellidos = "H."
+    var edad : Int = 23
+    var sexo = "Masculino"
+    var estadoCivil = "Soltero"
+    var altura : Double = 1.75
+    var activo : Boolean = true
+
+    fun dowhile(){
+        var contador = 10
+        while (contador > 1){
+
+        }
+        do{
+
+        }while(contador>3)
+
+    }
+
+    fun bucleFor(){
+        val notas = intArrayOf(13,20,4,6,8,10)
+        for(nota: Int in notas){
+            imprimir(nota.toString())
+        }
+        for((indice, valor) in notas.withIndex()){
+            imprimir("Nota $indice es igual $valor")
+        }
+        for(numero in 5..10){
+            imprimir("Numero $numero")
+        }
+
+    fun arreglosKotlin(){
+        val arrayNumeros: IntArray = intArrayOf(12,12,5)
+        val paises = ArrayList<String>()
+        paises.add("peru")
+        paises.add("argentina")
+        paises.add("chile")
+    }
+
+    fun imprimir(mensaje:String) {
+        Log.i("imprimir",mensaje)
+    }
+    fun operaciones (numero1: Int, numero2: Int,
+                     operacion: String) : String {
+        return "Resultado final"
+    }
+
+}
+
+
