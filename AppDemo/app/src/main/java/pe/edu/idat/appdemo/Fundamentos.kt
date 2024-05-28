b061a026fb892bb85ab0d4ec3c5c337fe4db55b0
DarkDeveloper25 <darkdeveloper259@gmail.com>
2024-05-27 19:47:03 -0500

    p2

--- AppDemo/app/src/main/java/pe/edu/idat/appdemo/Fundamentos.kt
+++ AppDemo/app/src/main/java/pe/edu/idat/appdemo/Fundamentos.kt
@@ -12,6 +12,25 @@ class Fundamentos {
     var altura : Double = 1.75
     var activo : Boolean = true
 
+    fun ejemplosLambda() {
+        val numeros = arrayListOf(9, 10, 30, 40, 5)
+        val numerosPares = numeros.filter { n -> n % 2 == 0 }
+
+        val miFuncionMultiplicacion = fun(n1: Int, n2: Int): Int {
+            return n1 * n2
+        }
+
+        // Aquí puedes usar o imprimir los resultados si lo deseas
+        println("Números pares: $numerosPares")
+        println("Multiplicación de 5 y 10: ${miFuncionMultiplicacion(5, 10)}")
+    }
+
+    private fun operaciones(x:Int, z:Int, funcion:(Int, Int) -> Int):Int {
+
+
+    }
+
+
     fun dowhile(){
         var contador = 10
         while (contador > 1){
@@ -35,22 +54,22 @@ class Fundamentos {
             imprimir("Numero $numero")
         }
 
-    fun arreglosKotlin(){
-        val arrayNumeros: IntArray = intArrayOf(12,12,5)
-        val paises = ArrayList<String>()
-        paises.add("peru")
-        paises.add("argentina")
-        paises.add("chile")
-    }
+        fun arreglosKotlin(){
+            val arrayNumeros: IntArray = intArrayOf(12,12,5)
+            val paises = ArrayList<String>()
+            paises.add("peru")
+            paises.add("argentina")
+            paises.add("chile")
+        }
 
-    fun imprimir(mensaje:String) {
-        Log.i("imprimir",mensaje)
-    }
-    fun operaciones (numero1: Int, numero2: Int,
-                     operacion: String) : String {
-        return "Resultado final"
-    }
+        fun imprimir(mensaje:String) {
+            Log.i("imprimir",mensaje)
+        }
+        fun operaciones (numero1: Int, numero2: Int,
+                         operacion: String) : String {
+            return "Resultado final"
+        }
 
-}
+    }
 
 
